45caf7e52b8705c3006ba1c8f05bf2bb1db03997
Sano <sanasar.hambardzumyan@gmail.com>
2022-11-22 22:02:09 +0400

    Corrections

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,11 +3,11 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_One = 1;
+        int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
-        final int variableFour = 4;
-        final int variableFive = 5;
+        final int VARIABLEFOUR = 4;
+        final int VARIABLEFIVE = 5;
         int variableSix = 6;
         int variableSeven = 7;
         int variableEight = 8;
